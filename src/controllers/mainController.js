b43d41f27023f86d481a3705a8344fa8ff6ae2ab
Eric <menaericdaniel@gmail.com>
2023-02-27 16:36:37 -0300

    fix show price with toThousand()

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,7 +4,7 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".").toFixed(0);
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
